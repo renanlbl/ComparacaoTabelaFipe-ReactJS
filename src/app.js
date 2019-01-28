96d5036135bfc1b1ef2c4c0a466de509d2e9c6ad
renanlblu <cursosprogramacoes@gmail.com>
2019-01-28 00:13:35 -0200

    Estrutura inicial webpack + react

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Estado da aplicação</h2>
+            </div>
+        )
+    }
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = {
+    devtool: 'source-map',
+    entry: [        
+        path.join(__dirname, 'src', 'index')
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/dist/'
+    },
+  
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            include: /src/,
+            use: [{
+                loader: 'babel-loader'
+            }]
+            
+        }]
+    },
+    performance: {
+        hints: false
+    }, 
+    optimization: {
+        minimize: false
+    }
+}
