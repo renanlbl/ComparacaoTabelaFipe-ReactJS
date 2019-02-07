8ef0dd1d576b198f371dbd19d05923d4b6e35a04
renanlblu <cursosprogramacoes@gmail.com>
2019-02-07 20:32:36 -0200

    Add script Start

---
+++ server.js
@@ -0,0 +1,16 @@
+const webpack = require('webpack')
+const WebpackDevServer = require('webpack-dev-server')
+const config = require('./webpack.config')
+
+new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback: true,
+  stats: { colors: true }
+}).listen(3000, (err) => {
+  if (err) {
+    return console.log(err)
+  }
+
+  console.log('Listening on http://localhost:3000')
+})
