814bf556548e67921f9db90bb993f2e16088fda3
renanlblu <cursosprogramacoes@gmail.com>
2019-02-08 14:33:07 -0200

    Teste heroku

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app'
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {urls} from "../utilsUrls";
 
 ReactDOM.render(
-    <App />,
+    <Router>
+        <Route path={urls.home.path} component={App}/>
+    </Router>,
     document.querySelector('[data-js="app"]')
 )
---
+++ utilsUrls.js
@@ -0,0 +1,3 @@
+export const urls = {
+    home: { name: 'Home', path:'/'}   
+}
